0dbd437e9f8edc45e5a3e16a7c351512b4ddbf77
danison00 <danison.santos00@gmail.com>
2023-11-04 23:29:24 -0300

    add cargo controller

--- src/main/java/com/dan/br/cadastrodefuncionario/web/controllers/CargoController.java
+++ src/main/java/com/dan/br/cadastrodefuncionario/web/controllers/CargoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dan.br.cadastrodefuncionario.model.Cargo;
 import com.dan.br.cadastrodefuncionario.service.CargoService;
 import com.dan.br.cadastrodefuncionario.web.Converters.Conversor;
